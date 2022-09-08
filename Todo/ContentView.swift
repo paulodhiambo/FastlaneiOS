8763eef49b54ae1e8f556c2f53461cf5600b6869
Paul Odhiambo <paulodhiambo962@gmail.com>
2022-09-08 22:17:58 +0300

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Paul Odhiambo on 08/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Paul Odhiambo on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
